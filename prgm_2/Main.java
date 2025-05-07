b639dc011f47f4872a9511859852cc973a6a24bd
sherly-23 <sherlyyamarthy@gmail.com>
2025-05-07 17:02:11 +0530

    to main

--- prgm_2/Main.java
+++ prgm_2/Main.java
@@ -1,5 +1,4 @@
-
-    import java.time.LocalDate;
+import java.time.LocalDate;
 import java.time.Period;
 
 class Student {
@@ -24,5 +23,3 @@ public class Main {
         s.displayInfo();
     }
 }
-
-
